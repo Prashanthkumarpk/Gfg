ae231e9c39c9e27262c4096fff6a35ef0845db0f
PrasanthKumar Yernagula <86708678+Prashanthkumarpk@users.noreply.github.com>
2023-09-07 23:21:03 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Total count/total-count.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int totalCount(int arr[], int n, int k) {
+        // code here
+        
+         int totalCount = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            totalCount += arr[i]/k;
+            totalCount += arr[i]%k == 0 ? 0 : 1;
+        }
+        
+        return totalCount;
+    
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.totalCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
