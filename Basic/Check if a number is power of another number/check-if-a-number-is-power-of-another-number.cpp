82f703a33ec0fb069c7753786cf6d5acb8c825b6
PrasanthKumar Yernagula <86708678+Prashanthkumarpk@users.noreply.github.com>
2023-09-09 22:56:16 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check if a number is power of another number/check-if-a-number-is-power-of-another-number.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int isPowerOfAnother(long long X, long long Y){
+        // code here 
+        
+        if ( X == 1 ) return Y == 1;
+    while (!( Y % X )) Y /= X;
+    return Y == 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long int X, Y;
+        cin >> X >> Y;
+        Solution ob;
+        cout << ob.isPowerOfAnother(X,Y) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
